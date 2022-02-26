f692264fe71030f1eb20443549212003df5089b2
Sebastian Górski <lambda.zielony@protonmail.ch>
2022-02-26 12:59:43 +0100

    Matplotlib example

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 12])
+ypoints = np.array([-1, 1])
+
+x = np.arange(0,4*np.pi,0.1)   # start,stop,step
+y = np.sin(x)
+
+plt.plot(x, y, xpoints, ypoints)
+plt.show()
+
 
 print (sys.version)
 
