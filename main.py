31b3376d0b2137ff633f4ce684d9a0b3e9cb062d
Sebastian Górski <lambda.zielony@protonmail.ch>
2022-02-26 13:31:43 +0100

    Matplotlib example

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import sys
-import matplotlib.pyplot as plt
-import numpy as np
-
-xpoints = np.array([0, 12])
-ypoints = np.array([-1, 1])
-
-x = np.arange(0,4*np.pi,0.1)   # start,stop,step
-y = np.sin(x)
-
-plt.plot(x, y, xpoints, ypoints)
-plt.show()
-
+import math as m
 
 print (sys.version)
+
+a = 15
+print(a)
+print(type(a))
+
+b = 'wizualizacja danych'
+print(b)
+print(type(b))
+
+c = 15.5
+print(c)
+print(type(c))
+
+d = 3+2j
+print(d)
+print(type(d))
+
+# this is a comment
+
+e = 7
+f = 3
+
+print(e // f)
+print(e ** f)
+for i in range(10):
+    print(m.pow(2, i))
 
 
